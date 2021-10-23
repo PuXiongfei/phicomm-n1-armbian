10bf336bb9585cc2989833e7aa0bc9060c26a2a1
PuXiongfei <pu.xiongfei@qq.com>
2021-10-24 00:21:16 +0800

    update config

--- override/config/bootscripts/phicomm-n1/boot-phicomm-n1.cmd
+++ override/config/bootscripts/phicomm-n1/boot-phicomm-n1.cmd
@@ -1,6 +1,6 @@
 setenv bootcmd 'run start_autoscript; run storeboot'
 setenv start_autoscript 'if mmcinfo; then run start_mmc_autoscript; fi; if usb start; then run start_usb_autoscript; fi; run start_emmc_autoscript'
-setenv start_emmc_autoscript 'if ext4load mmc 1 ${loadaddr} /boot/s905_autoscript || fatload mmc 1 ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi'
+setenv start_emmc_autoscript 'if ext4load mmc 1 ${loadaddr} /boot/emmc_autoscript || fatload mmc 1 ${loadaddr} emmc_autoscript; then autoscr ${loadaddr}; fi'
 setenv start_mmc_autoscript 'if ext4load mmc 0 ${loadaddr} /boot/s905_autoscript || fatload mmc 0 ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi'
 setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load usb ${devnum} ${loadaddr} /boot/s905_autoscript || fatload usb ${devnum} ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi; done'
 saveenv
