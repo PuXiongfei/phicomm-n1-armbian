b251e224bc259ccb0f090b4500d164f5d343c885
PuXiongfei <pu.xiongfei@qq.com>
2021-10-24 00:17:44 +0800

    update config

--- override/config/bootscripts/phicomm-n1/boot-aml_autoscript.cmd
+++ override/config/bootscripts/phicomm-n1/boot-aml_autoscript.cmd
@@ -1,8 +1,8 @@
 setenv bootcmd 'run start_autoscript; run storeboot'
 setenv start_autoscript 'if mmcinfo; then run start_mmc_autoscript; fi; if usb start; then run start_usb_autoscript; fi; run start_emmc_autoscript'
-setenv start_emmc_autoscript 'for devnum in 0 1 2 3; do if ext4load mmc ${devnum} 1020000 /boot/emmc_autoscript || fatload mmc ${devnum} 1020000 emmc_autoscript; then autoscr 1020000; fi; done'
-setenv start_mmc_autoscript 'for devnum in 0 1 2 3; do if ext4load mmc ${devnum} 1020000 /boot/s905_autoscript || fatload mmc ${devnum} 1020000 s905_autoscript; then autoscr 1020000; fi; done'
-setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load mmc ${devnum} 1020000 /boot/s905_autoscript || fatload mmc ${devnum} 1020000 s905_autoscript; then autoscr 1020000; fi; done'
+setenv start_emmc_autoscript 'if ext4load mmc 1 ${loadaddr} /boot/s905_autoscript || fatload mmc 1 ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi'
+setenv start_mmc_autoscript 'if ext4load mmc 0 ${loadaddr} /boot/s905_autoscript || fatload mmc 0 ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi'
+setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load usb ${devnum} ${loadaddr} /boot/s905_autoscript || fatload usb ${devnum} ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi; done'
 setenv upgrade_step 2
 saveenv
 sleep 1
