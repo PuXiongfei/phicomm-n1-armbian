55ee7c6caa97ab4e59dbd0da9ac8f6abb9939ef3
PuXiongfei <pu.xiongfei@qq.com>
2021-11-20 19:17:40 +0800

    update config

--- override/config/bootscripts/phicomm-n1/boot-aml_autoscript.cmd
+++ override/config/bootscripts/phicomm-n1/boot-aml_autoscript.cmd
@@ -1,8 +1,9 @@
+env default -a
 setenv bootcmd 'run start_autoscript; run storeboot'
 setenv start_autoscript 'if mmcinfo; then run start_mmc_autoscript; fi; if usb start; then run start_usb_autoscript; fi; run start_emmc_autoscript'
-setenv start_emmc_autoscript 'if ext4load mmc 1 ${loadaddr} /boot/emmc_autoscript || fatload mmc 1 ${loadaddr} emmc_autoscript; then autoscr ${loadaddr}; fi'
-setenv start_mmc_autoscript 'if ext4load mmc 0 ${loadaddr} /boot/s905_autoscript || fatload mmc 0 ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi'
-setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load usb ${devnum} ${loadaddr} /boot/s905_autoscript || fatload usb ${devnum} ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi; done'
+setenv start_emmc_autoscript 'if ext4load mmc 1 0x08000000 /boot/emmc_autoscript || fatload mmc 1 0x08000000 emmc_autoscript; then autoscr 0x08000000; fi'
+setenv start_mmc_autoscript 'if ext4load mmc 0 0x08000000 /boot/s905_autoscript || fatload mmc 0 0x08000000 s905_autoscript; then autoscr 0x08000000; fi'
+setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load usb ${devnum} 0x08000000 /boot/s905_autoscript || fatload usb ${devnum} 0x08000000 s905_autoscript; then autoscr 0x08000000; fi; done'
 setenv upgrade_step 2
 saveenv
 sleep 1
--- override/config/bootscripts/phicomm-n1/boot-mainline.cmd
+++ override/config/bootscripts/phicomm-n1/boot-mainline.cmd
@@ -1,8 +1,8 @@
 setenv bootcmd 'run start_autoscript; run storeboot'
 setenv start_autoscript 'if mmcinfo; then run start_mmc_autoscript; fi; if usb start; then run start_usb_autoscript; fi; run start_emmc_autoscript'
-setenv start_emmc_autoscript 'if ext4load mmc 1 ${loadaddr} /boot/emmc_autoscript || fatload mmc 1 ${loadaddr} emmc_autoscript; then autoscr ${loadaddr}; fi'
-setenv start_mmc_autoscript 'if ext4load mmc 0 ${loadaddr} /boot/s905_autoscript || fatload mmc 0 ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi'
-setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load usb ${devnum} ${loadaddr} /boot/s905_autoscript || fatload usb ${devnum} ${loadaddr} s905_autoscript; then autoscr ${loadaddr}; fi; done'
+setenv start_emmc_autoscript 'if ext4load mmc 1 0x08000000 /boot/emmc_autoscript || fatload mmc 1 0x08000000 emmc_autoscript; then autoscr 0x08000000; fi'
+setenv start_mmc_autoscript 'if ext4load mmc 0 0x08000000 /boot/s905_autoscript || fatload mmc 0 0x08000000 s905_autoscript; then autoscr 0x08000000; fi'
+setenv start_usb_autoscript 'for devnum in 0 1 2 3; do if ext4load usb ${devnum} 0x08000000 /boot/s905_autoscript || fatload usb ${devnum} 0x08000000 s905_autoscript; then autoscr 0x08000000; fi; done'
 saveenv
 
 # default values
@@ -19,10 +19,12 @@ for devtype in "usb mmc"; do
             setenv prefix ""
         fi
 
+        echo "Current loader: ${loader}"
+        echo "Current prefix: ${prefix}"
+
         if test -e ${devtype} ${devnum} ${prefix}u-boot.bin; then
-            echo "${loader} ${devtype} ${devnum} ${load_addr} ${prefix}u-boot.bin"
-            ${loader} ${devtype} ${devnum} ${load_addr} ${prefix}u-boot.bin
-            go ${loadaddr}
+            echo "${loader} ${devtype} ${devnum} 0x08000000 ${prefix}u-boot.bin"
+            ${loader} ${devtype} ${devnum} 0x08000000 ${prefix}u-boot.bin && go 0x08000000
         else
             echo "Not found u-boot.bin"
         fi
