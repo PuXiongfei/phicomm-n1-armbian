52d932c8bb23ef934760a34aa05d9fcfce343690
PuXiongfei <pu.xiongfei@qq.com>
2021-11-23 20:46:42 +0800

    update config

---
+++ override/userpatches/customize-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# arguments: $RELEASE $LINUXFAMILY $BOARD $BUILD_DESKTOP
+#
+# This is the image customization script
+
+# NOTE: It is copied to /tmp directory inside the image
+# and executed there inside chroot environment
+# so don't reference any files that are not already installed
+
+# NOTE: If you want to transfer files between chroot and host
+# userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
+# The sd card's root path is accessible via $SDCARD variable.
+
+RELEASE=$1
+LINUXFAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
+Main() {
+	# reconfigure tzdata
+	timedatectl set-timezone "Asia/Shanghai"
+	dpkg-reconfigure --frontend=noninteractive tzdata >/dev/null 2>&1
+} # Main
+
+Main "$@"
