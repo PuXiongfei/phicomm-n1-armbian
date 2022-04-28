bd678e64c4aace0ac923f6d4b843f7eafae58864
PuXiongfei <pu.xiongfei@qq.com>
2022-04-28 18:52:15 +0800

    update config

--- override/userpatches/bootscripts/boot-aml_autoscript.cmd
+++ override/userpatches/bootscripts/boot-aml_autoscript.cmd
@@ -1,8 +1,8 @@
 env default -a
 setenv bootcmd 'run start_autoscript; run storeboot'
 setenv start_autoscript 'if usb start; then run start_usb_autoscript; fi; run start_emmc_autoscript'
-setenv start_emmc_autoscript 'if ext4load mmc 1 0x08000000 /boot/emmc_autoscript || fatload mmc 1 0x08000000 emmc_autoscript; then autoscr 0x08000000; fi'
-setenv start_usb_autoscript 'if ext4load usb 0 0x08000000 /boot/s905_autoscript || fatload usb 0 0x08000000 s905_autoscript; then autoscr 0x08000000; fi'
+setenv start_emmc_autoscript 'if ext4load mmc 1 0x08000000 /boot/emmc_autoscript; then autoscr 0x08000000; fi'
+setenv start_usb_autoscript 'if fatload usb 0 0x08000000 s905_autoscript; then autoscr 0x08000000; fi'
 setenv upgrade_step 2
 saveenv
 sleep 1
