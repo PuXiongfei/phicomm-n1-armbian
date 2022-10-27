fc15344193860394835e71aa1567c2e344bfbfd8
PuXiongfei <pu.xiongfei@qq.com>
2022-10-27 15:33:03 +0800

    update config

--- override/userpatches/customize-image.sh
+++ override/userpatches/customize-image.sh
@@ -21,13 +21,13 @@ Main() {
 	# phicomm-n1 use mainline u-boot to overload boot
 	if [[ -f /usr/lib/u-boot/platform_install.sh ]]; then
 		DIR=$(grep "DIR" /usr/lib/u-boot/platform_install.sh | awk -F '=' '{print $2}')
-		echo "u-boot path: $DIR"
+		echo "mainline u-boot path: $DIR"
 		# rename $DIR/u-boot.bin to u-boot-mainline.bin
 		echo "rename $DIR/u-boot.bin to u-boot-mainline.bin"
 		mv -f $DIR/u-boot.bin $DIR/u-boot-mainline.bin
 		# copy $DIR/u-boot-mainline.bin to /boot/u-boot.bin
 		echo "copy $DIR/u-boot-mainline.bin to /boot/u-boot.bin"
-		cp -af $DIR/u-boot-mainline.bin /boot/u-boot.bin
+		\cp -af $DIR/u-boot-mainline.bin /boot/u-boot.bin
 	fi
 
 	# modify armbian-install for phicomm-n1
@@ -35,18 +35,34 @@ Main() {
 	sed -i 's/FIRSTSECTOR=.*/FIRSTSECTOR=1638400/' /usr/sbin/armbian-install
 	grep "FIRSTSECTOR=" /usr/sbin/armbian-install
 
-	echo "add backup bootloader when armbian-install for phicomm-n1"
-	sed -i '/^emmccheck=.*/a\[[ -n $emmccheck ]] && dd if=$emmccheck of=$DIR/u-boot.bin bs=1M count=4 conv=fsync >/dev/null 2>&1\n[[ -f $DIR/u-boot.bin ]] && echo "bootloader backup $DIR/u-boot.bin" >> $logfile' /usr/sbin/armbian-install
+	echo "add backup bootloader when armbian-install"
+	sed -i '/^emmccheck=.*/a\[[ -n $emmccheck && $BOARD_NAME == "phicomm-n1" ]] && dd if=$emmccheck of=$DIR/u-boot.bin bs=1M count=4 conv=fsync >/dev/null 2>&1\n[[ -f $DIR/u-boot.bin ]] && echo "phicomm-n1 bootloader backup to $DIR/u-boot.bin" >> $logfile' /usr/sbin/armbian-install
 	grep "u-boot.bin" /usr/sbin/armbian-install
+
+	echo "disable 64bit when mkfs.ext4"
+	sed -i "/^create_armbian/i\[[ \$BOARD_NAME == \"phicomm-n1\" ]] && mkopts[ext4]='-O ^64bit -qF' && echo \"mkopts[ext4]=\${mkopts[ext4]}\" >> \$logfile" /usr/sbin/armbian-install
+	grep "mkopts\[ext4\]=" /usr/sbin/armbian-install
+
+	echo "set parted mklabel msdos if empty"
+	sed -i '/\tPART_TABLE_TYPE/a\    [[ -z $PART_TABLE_TYPE && $BOARD_NAME == "phicomm-n1" ]] && PART_TABLE_TYPE="msdos" && echo "PART_TABLE_TYPE=$PART_TABLE_TYPE" >> $logfile' /usr/sbin/armbian-install
+	grep "PART_TABLE_TYPE=" /usr/sbin/armbian-install
 
 	# modify nand-sata-install for phicomm-n1
 	echo "modify parted SECTOR (800 * 1024 * 1024) / 512"
 	sed -i 's/FIRSTSECTOR=.*/FIRSTSECTOR=1638400/' /usr/sbin/nand-sata-install
 	grep "FIRSTSECTOR=" /usr/sbin/nand-sata-install
 
-	echo "add backup bootloader when nand-sata-install for phicomm-n1"
-	sed -i '/^emmccheck=.*/a\[[ -n $emmccheck ]] && dd if=$emmccheck of=$DIR/u-boot.bin bs=1M count=4 conv=fsync >/dev/null 2>&1\n[[ -f $DIR/u-boot.bin ]] && echo "bootloader backup $DIR/u-boot.bin" >> $logfile' /usr/sbin/nand-sata-install
+	echo "add backup bootloader when nand-sata-install"
+	sed -i '/^emmccheck=.*/a\[[ -n $emmccheck && $BOARD_NAME == "phicomm-n1" ]] && dd if=$emmccheck of=$DIR/u-boot.bin bs=1M count=4 conv=fsync >/dev/null 2>&1\n[[ -f $DIR/u-boot.bin ]] && echo "phicomm-n1 bootloader backup to $DIR/u-boot.bin" >> $logfile' /usr/sbin/nand-sata-install
 	grep "u-boot.bin" /usr/sbin/nand-sata-install
+
+	echo "disable 64bit when mkfs.ext4"
+	sed -i "/^create_armbian/i\[[ \$BOARD_NAME == \"phicomm-n1\" ]] && mkopts[ext4]='-O ^64bit -qF' && echo \"mkopts[ext4]=\${mkopts[ext4]}\" >> \$logfile" /usr/sbin/nand-sata-install
+	grep "mkopts\[ext4\]=" /usr/sbin/nand-sata-install
+
+	echo "set parted mklabel msdos if empty"
+	sed -i '/\tPART_TABLE_TYPE/a\    [[ -z $PART_TABLE_TYPE && $BOARD_NAME == "phicomm-n1" ]] && PART_TABLE_TYPE="msdos" && echo "PART_TABLE_TYPE=$PART_TABLE_TYPE" >> $logfile' /usr/sbin/nand-sata-install
+	grep "PART_TABLE_TYPE=" /usr/sbin/nand-sata-install
 
 	# install docker
 	echo "install docker"
