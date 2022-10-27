2bae9d82422d4b7143210ed2d41889911502e616
PuXiongfei <pu.xiongfei@qq.com>
2022-10-27 11:20:33 +0800

    update config

--- override/userpatches/bootscripts/boot-emmc_autoscript.cmd
+++ override/userpatches/bootscripts/boot-emmc_autoscript.cmd
@@ -1,5 +1,5 @@
-setenv bootcmd 'run start_autoscript; run storeboot'
-setenv start_autoscript 'if usb start; then run start_usb_autoscript;else run start_emmc_autoscript;fi;'
+setenv bootcmd 'run start_autoscript;run storeboot;'
+setenv start_autoscript 'if usb start; then run start_usb_autoscript;fi;run start_emmc_autoscript'
 setenv start_emmc_autoscript 'if ext4load mmc 1 0x08000000 /boot/emmc_autoscript; then autoscr 0x08000000;else if fatload mmc 1 0x08000000 emmc_autoscript; then autoscr 0x08000000;else echo "load emmc_autoscript failed";fi;fi;'
 setenv start_usb_autoscript 'if ext4load usb 0 0x08000000 /boot/s905_autoscript; then autoscr 0x08000000;else if fatload usb 0 0x08000000 s905_autoscript; then autoscr 0x08000000;else echo "load s905_autoscript failed";fi;fi;'
 saveenv
